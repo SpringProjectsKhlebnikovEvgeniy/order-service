a506ae96f9b1aa34758aa62c5de9c3569c9e64b8
KhlebnikovEvgeniy <khebnikove@gmail.com>
2023-07-22 19:03:56 +0300

    Initial commit. Order Service.

---
+++ src/test/java/sprojects/productservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sprojects.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
