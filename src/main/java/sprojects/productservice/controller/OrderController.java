93426ce988fafbbab43e3ee7a5a9e51baeb5cdec
KhlebnikovEvgeniy <khebnikove@gmail.com>
2023-07-25 23:16:21 +0300

    Controller. OrderController.

--- src/main/java/sprojects/productservice/controller/OrderController.java
+++ src/main/java/sprojects/productservice/controller/OrderController.java
@@ -1,18 +1,27 @@
 package sprojects.productservice.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
 import sprojects.productservice.dto.OrderRequest;
+import sprojects.productservice.service.OrderService;
 
 @RestController
 @RequestMapping("/api/order")
+@RequiredArgsConstructor
 public class OrderController {
 	
+	private final OrderService orderService;
+	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public String placeOrder(@RequestBody OrderRequest orderRequest) {
+		orderService.placeOrder(orderRequest);
 		return "Order Placed Successfully";
 	}
 
